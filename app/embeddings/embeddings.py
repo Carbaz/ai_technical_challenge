b1d2b44b0024fe51ff93c85118141b954e50dbd4
Carlos Bazaga <cabazaga@gmail.com>
2025-10-15 21:40:25 +0200

    Refactor config and embeddings modules to enhance logging and add PDF processing level configuration.
    This avoids multimodal calls that may be not available on models API service.

--- app/embeddings/embeddings.py
+++ app/embeddings/embeddings.py
@@ -12,7 +12,7 @@ from langchain_core.documents import Document
 from langchain_core.embeddings import Embeddings
 from langchain_openai import OpenAIEmbeddings
 
-from app.config import LLM_API_KEY, LLM_API_URL
+from app.config import LLM_API_KEY, LLM_API_URL, PDF_PROCESSING_LEVEL
 
 from .llm_chunker import chunk_from_directory_using_llm
 
@@ -64,30 +64,51 @@ def embed_documents(docs: list[Document], model: Embeddings, db_host, db_port):
                                  host=db_host, port=db_port)
 
 
+def chunk_directory_text(directory, chunk_size, chunk_overlap):
+    """Chunk text documents from directory."""
+    _logger.info(f'LOADING TEXT DOCUMENTS FROM "{directory}"')
+    text_documents = load_text_from_directory(directory)
+    _logger.info(f'CHUNKING TEXT DOCUMENTS')
+    return chunk_documents(text_documents, chunk_size, chunk_overlap)
+
+
+def chunk_directory_pdf(directory, chunk_size, chunk_overlap):
+    """Chunk PDF documents from directory."""
+    _logger.info(f'LOADING PDF DOCUMENTS FROM "{directory}" AT {PDF_PROCESSING_LEVEL} LEVEL')
+    match PDF_PROCESSING_LEVEL:
+        case 'LOW':
+            _logger.info(f'LOADING PDF DOCUMENTS FROM "{directory}"')
+            pdf_documents = load_pdf_from_directory(directory)
+            _logger.info(f'CHUNKING PDF DOCUMENTS')
+            return chunk_documents(pdf_documents, chunk_size, chunk_overlap)
+        case 'MEDIUM':
+            _logger.info(f'LOADING PDF DOCUMENTS WITH OCR FROM "{directory}"')
+            pdf_documents = load_pdf_from_directory_with_ocr(directory)
+            _logger.info(f'CHUNKING PDF DOCUMENTS')
+            return chunk_documents(pdf_documents, chunk_size, chunk_overlap)
+        case 'HIGH':
+            _logger.info(f'LOADING AND CHUNKING WITH LLM FROM "{directory}')
+            return chunk_from_directory_using_llm(directory, chunk_size=chunk_size,
+                                                  chunk_overlap=chunk_overlap)
+
+
 def embed_directory(directory, metadata, model_name,
                     chunk_size, chunk_overlap, db_host, db_port):
     """Embed documents from directory."""
-    embedding_model = OpenAIEmbeddings(model=model_name,
-                                       api_key=LLM_API_KEY, base_url=LLM_API_URL)
-
-    _logger.info(f'LOADING TEXT DOCUMENTS FROM "{directory}"')
-    text_documents = load_text_from_directory(directory)
-
-    _logger.info(f'CHUNKING TEXT DOCUMENTS')
-    text_chunks = chunk_documents(text_documents, chunk_size, chunk_overlap)
-
-    _logger.info(f'LOADING PDF DOCUMENTS FROM "{directory}"')
-    pdf_chunks = chunk_from_directory_using_llm(directory, chunk_size=chunk_size,
-                                                chunk_overlap=chunk_overlap)
-
+    # Process text and Markdown documents.
+    text_chunks = chunk_directory_text(directory, chunk_size, chunk_overlap)
+    # Process PDF documents.
+    pdf_chunks = chunk_directory_pdf(directory, chunk_size, chunk_overlap)
+    # Combine all chunks.
     chunks = text_chunks + pdf_chunks
     if not chunks:
         _logger.info(f'NO DOCUMENTS FOUND AT "{directory}"')
         return
-
+    # Update metadata and embed all chunks.
     _logger.info(f'UPDATING METADATA')
     chunks = update_metadata(chunks, metadata)
-
+    embedding_model = OpenAIEmbeddings(model=model_name,
+                                       api_key=LLM_API_KEY, base_url=LLM_API_URL)
     _logger.info(f'EMBEDDING DOCUMENTS')
     embed_documents(chunks, embedding_model, db_host, db_port)
 
