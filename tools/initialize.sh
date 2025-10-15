6a8be929d1c6740375efa7c4fb5ab611171ccd73
Carlos Bazaga <cabazaga@gmail.com>
2025-10-15 11:10:35 +0200

    Update initialize.sh to use pipenv for running Python scripts

--- tools/initialize.sh
+++ tools/initialize.sh
@@ -3,9 +3,9 @@
 # set -exuo pipefail
 
 echo "Cleaning up DB..."
-python -Bm tools.cleanup_chroma
+pipenv run python -Bm tools.cleanup_chroma
 
 echo "Initializing DB..."
-python -Bm tools.embed_company -s policies/United -c United
-python -Bm tools.embed_company -s policies/Delta -c Delta
-python -Bm tools.embed_company -s policies/AmericanAirlines -c AmericanAirlines
+pipenv run python -Bm tools.embed_company -s policies/United -c United
+pipenv run python -Bm tools.embed_company -s policies/Delta -c Delta
+pipenv run python -Bm tools.embed_company -s policies/AmericanAirlines -c AmericanAirlines
